46939829ce38821ce6dbb52567a7ad323a2dc953
jeean0668 <gimjian@gimjian-ui-MacBook.local>
2021-10-07 14:54:31 +0900

    modified

--- graph/BFS/gold/1938.py
+++ graph/BFS/gold/1938.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 import sys
 from collections import deque
 input = sys.stdin.readline
 
+
 def getInput():
-    global n, graph, start, end
+    global n, graph, start, end, end_dir
     n = int(input())
     graph = []
     start, end = [], []
@@ -15,6 +17,10 @@ def getInput():
                 start.append([i, j])
             elif graph[i][j] == "E":
                 end.append([i, j])
+    if end[0][0] - end[1][0] == 1:
+        end_dir = 1
+    else:
+        end_dir = 0
 def canmoveRight(center, dir):
     global graph
     
@@ -22,16 +28,21 @@ def canmoveRight(center, dir):
     if dir == 0:
         first_y, first_x = center[0], center[1] - 1
         last_y, last_x = center[0], center[1] + 1
-        if graph[last_y][last_x + 1] != 1:
-            return True
-        return False
+        if not(0<= last_x + 1 < n):
+            return False
+        if graph[last_y][last_x + 1] == '1':
+            return False
+        return True
     # vertical 할경우
     elif dir == 1:
         first_y, first_x = center[0]-1, center[1]
         last_y, last_x = center[0]+1, center[1]
-        if graph[first_y][first_x + 1] != 1 and graph[center[0]][center[1] + 1] != 1 and graph[last_y][last_x + 1] != 1:
-            return True
-        return False 
+        if not(0<=first_x+1<n or 0<=center[1]+1<n or 0<=last_x+1<n):
+            return False
+        if graph[first_y][first_x + 1] == '1' or graph[center[0]][center[1] + 1] == '1' or graph[last_y][last_x + 1] == '1':
+            return False
+        return True
+    
 def canmoveLeft(center, dir):
     global graph
     
@@ -39,20 +50,44 @@ def canmoveLeft(center, dir):
     if dir == 0:
         first_y, first_x = center[0], center[1] - 1
         last_y, last_x = center[0], center[1] + 1
-        if graph[first_y][first_x - 1] != 1:
-            return True
-        return False
+        if not(0<=first_x-1<n):
+            return False
+        if graph[first_y][first_x - 1] == '1':
+            return False
+        return True
     # vertical 할경우
     elif dir == 1:
         first_y, first_x = center[0]-1, center[1]
         last_y, last_x = center[0]+1, center[1]
-        if graph[first_y][first_x - 1] != 1 and graph[center[0]][center[1] - 1] != 1 and graph[last_y][last_x - 1] != 1:
-            return True
-        return False 
-def canRotate(center, dir):
-    if canmoveLeft(center, dir) and canmoveRight(center, dir):
+        if not(0<=first_x-1<n or 0<=center[1]-1<n or 0<=last_x-1<n):
+            return False
+        if graph[first_y][first_x - 1] == '1' or graph[center[0]][center[1] - 1] == '1' or graph[last_y][last_x - 1] == '1':
+            return False
         return True
-    return False
+def canRotate(center, dir):
+    if dir == 0:
+        first_y, first_x = center[0], center[1] - 1
+        last_y, last_x = center[0], center[1] + 1
+        if not(0<=first_y-1<n or 0<=center[0]-1<n or 0<=last_y-1<n):
+            return False
+        if not(0<=first_y+1<n or 0<=center[0]+1<n or 0<=last_y+1<n):
+            return False
+        if graph[first_y-1][first_x] == '1' or graph[center[0]-1][first_x] == '1' or graph[last_y - 1][last_x] == '1':
+            return False
+        if graph[first_y+1][first_x] == '1' or graph[center[0]+1][first_x] == '1' or graph[last_y + 1][last_x] == '1':
+            return False
+    if dir == 1:
+        first_y, first_x = center[0]-1, center[1]
+        last_y, last_x = center[0]+1, center[1]
+        if not(0<=first_x-1<n or 0<=center[1]-1<n or 0<=last_x-1<n):
+            return False
+        if not(0<=first_x+1<n or 0<=center[1]+1<n or 0<=last_x+1<n):
+            return False
+        if graph[first_y][first_x-1] == '1' or graph[center[0]][center[1]-1] == '1' or graph[last_y][last_x-1] == '1':
+            return False
+        if graph[first_y][first_x+1] == '1' or graph[center[0]][center[1]+1] == '1' or graph[last_y][last_x+1] == '1':
+            return False
+        
 def canmoveUp(center, dir):
     global graph
     
@@ -60,59 +95,68 @@ def canmoveUp(center, dir):
     if dir == 1:
         first_y, first_x = center[0]-1, center[1]
         last_y, last_x = center[0]+1, center[1]
-        if graph[first_y - 1][first_x] != 1:
-            return True
-        return False
+        if not(0<=first_y-1<n):
+            return False
+        if graph[first_y - 1][first_x] == '1':
+            return False
+        return True
     #horizontal 할 경우
     elif dir == 0:
         first_y, first_x = center[0], center[1] - 1
         last_y, last_x = center[0], center[1] + 1
-        if graph[first_y - 1][first_x] != 1 and graph[center[0] - 1][center[1]] != 1 and graph[last_y - 1][last_x] != 1:
-            return True
-        return False
+        if not(0<=first_y -1 < n or 0<=center[0] - 1 <n or 0<=last_y-1<n):
+            return False
+        if graph[first_y - 1][first_x] == '1' or graph[center[0] - 1][center[1]] == '1' or graph[last_y - 1][last_x] == '1':
+            return False
+        return True
 
 def canmoveDown(center, dir):
     global graph
-   
     # vertical 할경우
     if dir == 1:
         first_y, first_x = center[0]-1, center[1]
         last_y, last_x = center[0]+1, center[1]
-        if graph[last_y + 1][last_x] != 1:
-            return True
-        return False
+
+        if not(0<=last_y+1<n):
+            return False
+        if graph[last_y + 1][last_x] == '1':
+            return False
+        return True
     #horizontal 할 경우
     elif dir == 0:
         first_y, first_x = center[0], center[1]-1
         last_y, last_x = center[0], center[1]+1
-        if graph[first_y + 1][first_x] != 1 and graph[center[0] + 1][center[1]] != 1 and graph[last_y + 1][last_x] != 1:
-            return True
-        return False 
+        if not(0<=first_y+1<n or 0<=center[0]+1<n or 0<=last_y+1<n):
+            return False
+        if graph[first_y + 1][first_x] == '1' or graph[center[0] + 1][center[1]] == '1' or graph[last_y + 1][last_x] == '1':
+            return False
+        return True
 def solve():
-    global n, graph, start, end
+    global n, graph, start, end, end_dir
     queue = deque()
     # visited는 [center y][center x][vertical or horizontal] 로 선언
     visited = [[[False for _ in range(2)] for _ in range(n+1)] for _ in range(n+1)]
     # 시작을 horizontal하게 시작할때 
-    print(start[0][1], start[1][0])
     if abs(start[0][0] - start[1][0])== 1:
-        queue.append([start[1], 0, 0])
-        visited[start[1][0]][start[1][1]][0] = True
-    elif abs(start[0][1] - start[1][1]) == 1:
         queue.append([start[1], 0, 1])
         visited[start[1][0]][start[1][1]][1] = True
+    elif abs(start[0][1] - start[1][1]) == 1:
+        queue.append([start[1], 0, 0])
+        visited[start[1][0]][start[1][1]][0] = True
     
     while queue:
         center, cnt, dir = queue.popleft() # center 좌표, 갯수, 누워있는 방향
         y, x = center[0], center[1]
-       
-        if y == end[1][0] and x == end[1][1]:
-            return cnt
         print(y, x, dir)
+        if y == end[1][0] and x == end[1][1] and dir == end_dir:
+            return cnt
+        
         if canmoveLeft(center, dir):
-           
+            
             ny, nx = y, x-1
             if 0<=ny<n and 0<=nx<n:
+                if ny == 12 and nx == 10:
+                    print(y, x, ny, nx, dir)
                 if not visited[ny][nx][dir]:
                     queue.append([[ny, nx], cnt + 1, dir])
                     visited[ny][nx][dir] = True
@@ -120,6 +164,8 @@ def solve():
 
             ny, nx = y, x+1
             if 0<=ny<n and 0<=nx<n:
+                if ny == 12 and nx == 10:
+                    print(y, x, ny, nx, dir)
                 if not visited[ny][nx][dir]:
                     queue.append([[ny, nx], cnt + 1, dir])
                     visited[ny][nx][dir] = True
@@ -127,6 +173,8 @@ def solve():
           
             ny, nx = y-1, x
             if 0<=ny<n and 0<=nx<n:
+                if ny == 12 and nx == 10:
+                    print(y, x, ny, nx, dir)
                 if not visited[ny][nx][dir]:
                     queue.append([[ny, nx], cnt + 1, dir])
                     visited[ny][nx][dir] = True
@@ -134,6 +182,8 @@ def solve():
            
             ny, nx = y+1, x
             if 0<=ny<n and 0<=nx<n:
+                if ny == 12 and nx == 10:
+                    print(y, x, ny, nx, dir)
                 if not visited[ny][nx][dir]:
                     queue.append([[ny, nx], cnt + 1, dir])
                     visited[ny][nx][dir] = True
@@ -141,15 +191,17 @@ def solve():
          
             ny, nx = y, x
             if 0<=ny<n and 0<=nx<n:
-                print(ny, nx, dir)
-                print(visited[ny][nx])
-                if not visited[ny][nx][dir]:
-                    if dir == 1: 
-                        queue.append([[ny, nx], cnt + 1, 0])
-                        visited[ny][nx][dir] = True
-                    else:
-                        queue.append([[ny, nx], cnt + 1, 1])
-                        visited[ny][nx][dir] = True
+                if ny == 12 and nx == 10:
+                    print(y, x, ny, nx, dir)
+            
+                if dir == 1:
+                    n_dir = 0
+                else:
+                    n_dir = 1
+                if not visited[ny][nx][n_dir]:
+                    queue.append([[ny, nx], cnt + 1, n_dir])
+                    visited[ny][nx][n_dir] = True
+                
     return 0
 
 
--- graph/BFS/gold/2146.py
+++ graph/BFS/gold/2146.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+type error 이유가 뭐냐 대체 -> 병신같이 input에 list를 안붙여줘서 2시간이나 헤맸다
+시발 진짜 뭐하는짓이지? 
+"""
 import sys
 input = sys.stdin.readline
 from collections import deque
@@ -10,15 +14,17 @@ def makeGraph(n):
     graph = []
     # n x n 맵 생성
     for _ in range(n):
-        row = map(int, input().split())
+        row = list(map(int, input().split()))
         graph.append(row)
+
 def getInput():
     global n
     n = int(input())
     makeGraph(n)
-def changeMap(sy, sx, cnt):
+
+def changeMap(sy, sx):
     # 연결된 모든 지역의 숫자를 cnt로 바꿔준다.
-    global visited, n
+    global visited, n, cnt
     queue = deque()
     queue.append([sy, sx])
     graph[sy][sx] = cnt
@@ -31,19 +37,59 @@ def changeMap(sy, sx, cnt):
             nx = x + dx[i]
             if not(0<=ny<n and 0<=nx<n):
                 continue
-            if not visited[ny][nx] and not graph[ny][nx] != 0:
+            if graph[ny][nx] != 1:
+                continue
+            if not visited[ny][nx] :
                 graph[ny][nx] = cnt
                 queue.append([ny,nx])
                 visited[ny][nx] = True
+def findShortest(num):
+    # num 번째 island에서 다른 섬을 잇는 가장 짧은 길이의 다리를 반환한다.
+    global n, result
+    distance = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+    queue = deque()
+
+    for i in range(n):
+        for j in range(n):
+            # 연결된 모든 지역들을 queue에 넣는다. 
+            if graph[i][j] == num:
+                queue.append([i, j])
+                distance[i][j] = 0
+
+    while queue:
+        y, x = queue.popleft()
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if not(0<=ny<n and 0<=nx<n):
+                continue
+            if graph[ny][nx] > 0 and graph[ny][nx] != num:
+                result = min(result, distance[y][x])
+                return 
+            if graph[ny][nx] == 0 and distance[ny][nx] == -1:
+                distance[ny][nx] = distance[y][x] + 1
+                queue.append([ny, nx])
+
 
 def solve():
-    # 맵을 1, 2, 3.. 번호를 붙인다.
-    global visited
+   
+    global visited, result, cnt
     visited = [[False for _ in range(n+1)] for _ in range(n+1)]
     cnt = 1
+
+     # 맵을 1, 2, 3.. 번호를 붙인다.
     for i in range(n):
         for j in range(n):
             if graph[i][j] == 1 and not visited[i][j]:
-                changeMap(i, j, cnt)
+                changeMap(i, j)
                 cnt += 1
-    print(changeMap)
+   
+     # 특정 섬과 다른 섬, 두 섬 사이의 최단 거리를 구한다.
+    # 모든 섬에서 다른 섬들을 잇는 가장 짧은 다리의 길이를 구한다. 
+    result = sys.maxsize
+    for i in range(1, cnt):
+        findShortest(i)
+    print(result)
+    
+if __name__ == "__main__":
+    getInput()
+    solve()
